171c302b2d2871ea1be8240bada2c9edc180fe47
Vidya Trimbake <vidyatrimbake3@gmail.com>
2026-01-18 01:19:35 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/fintechledger/ledger/FintechLedgerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.fintechledger.ledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechLedgerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintechLedgerSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fintechledger/ledger/FintechLedgerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintechledger.ledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechLedgerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
